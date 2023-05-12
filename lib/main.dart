95a93facfe33ec2e26f3acbf50639bc4b66bca17
arrapiii <mohamadarrafi1304@gmail.com>
2023-05-12 11:27:14 +0700

    part4

--- lib/main.dart
+++ lib/main.dart
@@ -15,51 +15,51 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: "asiewdhiwehwieh",
       debugShowCheckedModeBanner: false,
-      home: CheckAuth(),
+      home: Home(),
       darkTheme: ThemeData(brightness: Brightness.dark),
       themeMode: ThemeMode.dark,
     );
   }
 }
 
-class CheckAuth extends StatefulWidget {
-  const CheckAuth({super.key});
+// class CheckAuth extends StatefulWidget {
+//   const CheckAuth({super.key});
 
-  @override
-  State<CheckAuth> createState() => _CheckAuthState();
-}
+//   @override
+//   State<CheckAuth> createState() => _CheckAuthState();
+// }
 
-class _CheckAuthState extends State<CheckAuth> {
-  bool isAuth = false;
+// class _CheckAuthState extends State<CheckAuth> {
+//   bool isAuth = false;
 
-  @override
-  void initState() {
-    super.initState();
-    _checkIfLoggedIn();
-  }
+//   @override
+//   void initState() {
+//     super.initState();
+//     _checkIfLoggedIn();
+//   }
 
-  void _checkIfLoggedIn() async {
-    SharedPreferences localStorage = await SharedPreferences.getInstance();
-    var token = localStorage.getString('token');
-    if (token != null) {
-      if (mounted) {
-        setState(() {
-          isAuth = true;
-        });
-      }
-    }
-  }
+//   void _checkIfLoggedIn() async {
+//     SharedPreferences localStorage = await SharedPreferences.getInstance();
+//     var token = localStorage.getString('token');
+//     if (token != null) {
+//       if (mounted) {
+//         setState(() {
+//           isAuth = true;
+//         });
+//       }
+//     }
+//   }
 
-  @override
-  Widget build(BuildContext context) {
-    Widget child;
-    if (isAuth) {
-      child = Home();
-    } else {
-      child = Login();
-    }
-    return Scaffold(
-      body: child,
-    );
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     Widget child;
+//     if (isAuth) {
+//       child = Home();
+//     } else {
+//       child = Login();
+//     }
+//     return Scaffold(
+//       body: child,
+//     );
+//   }
+// }
